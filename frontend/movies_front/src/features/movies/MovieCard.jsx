54a56f9f97620673f7ebeb694ebd8858778fc0aa
Jovana <jovanadjurdjevac08@gmail.com>
2024-06-28 18:00:04 +0200

    Movies by genre page

--- frontend/movies_front/src/features/movies/MovieCard.jsx
+++ frontend/movies_front/src/features/movies/MovieCard.jsx
@@ -1,20 +1,28 @@
 import "../../styles/components/MovieCard.scss";
 
 /* eslint-disable react/prop-types */
-function MovieCard({ poster, name, genres }) {
+function MovieCard({ movie }) {
+  const year = new Date(movie.release_date).getFullYear();
   return (
     <div className="movie__card">
       <img
-        src={poster}
-        alt={`${name} poster`}
+        src={movie.image}
+        alt={`${movie.title} poster`}
         className="movie__card--poster"
       />
-      <h3>{name}</h3>
-      <p>
-        {genres.map((genre) => (
-          <p key={genre.id}>{genre}</p>
-        ))}
-      </p>
+      <div className="movie__card--details">
+        <h3 className="movie__card--title">{movie.title}</h3>
+        <div className="movie__card--genres">
+          {movie.genres.map((genre) => (
+            <p key={genre.id}>{genre.name}</p>
+          ))}
+        </div>
+        <p className="movie__card--year">{year}</p>
+        <p className="movie__card--description">{movie.description}</p>
+      </div>
+      <div className="movie__card--rating">
+        <div className="rating__box">9.8</div>
+      </div>
     </div>
   );
 }
--- frontend/movies_front/src/pages/Genre.jsx
+++ frontend/movies_front/src/pages/Genre.jsx
@@ -1,23 +1,21 @@
 import MovieCard from "../features/movies/MovieCard";
 import { useMovies } from "../features/movies/useMovies";
 import { useLocation } from "react-router-dom";
+import "../styles/pages/Genre.scss";
 
 function Genre() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const genreId = searchParams.get("genre_id");
-  const genres = ["action", "drama", "comedy"];
 
   const { movies, isLoading } = useMovies(genreId);
-  if (isLoading) return <div>Loading...</div>;
   console.log(movies);
+  if (isLoading) return <div>Loading...</div>;
   return (
-    <div>
-      <MovieCard
-        poster="../bohemian.jpg"
-        name="Bohemian Rapsody"
-        genres={genres}
-      />
+    <div className="movie__grid">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 }
